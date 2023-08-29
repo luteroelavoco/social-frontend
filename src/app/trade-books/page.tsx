f0393ebc10a92db6f2402a4be22318083e8e4d76
luteroelavoco <luteroelavo90@gmail.com>
2023-08-29 03:46:03 -0300

    feat: trade page

---
+++ src/app/trade-books/page.tsx
@@ -0,0 +1,51 @@
+'use client'
+
+import { Box, Container, Typography, Divider } from '@mui/material'
+import AuthPage from '@/wrapper/Auth'
+import { useBookTrade } from '@/context/BookTrade'
+import TradeBookCard from '@/components/TradeBookCard'
+
+const TradeBooks: React.FC = () => {
+  const { booksTrade } = useBookTrade()
+
+  return (
+    <AuthPage>
+      <Container
+        sx={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          my: 4
+        }}
+      >
+        <Box
+          sx={{
+            position: 'relative',
+            width: '90%',
+            maxWidth: '650px',
+            background: 'white',
+            py: '16px',
+            borderRadius: '8px'
+          }}
+        >
+          <Typography
+            sx={{
+              fontSize: '24px',
+              fontWeight: 'bold',
+              px: '24px',
+              mb: '8px'
+            }}
+          >
+            Solicitações de troca
+          </Typography>
+          <Divider sx={{ borderWidth: 1 }} />
+          {booksTrade.reverse().map(bookTrade => (
+            <TradeBookCard key={bookTrade._id} bookTrade={bookTrade} />
+          ))}
+        </Box>
+      </Container>
+    </AuthPage>
+  )
+}
+
+export default TradeBooks
---
+++ src/components/TradeBookCard/index.tsx
@@ -0,0 +1,123 @@
+import { useBookTrade } from '@/context/BookTrade'
+import { useUser } from '@/context/User'
+import { BookTrade } from '@/types/bookTrade'
+import { User } from '@/types/user'
+import { Avatar, Box, Button, Stack, Typography, Divider } from '@mui/material'
+import { useSnackbar, SnackbarOrigin } from 'notistack'
+import moment from 'moment'
+
+const anchorOrigin: SnackbarOrigin = {
+  vertical: 'top',
+  horizontal: 'right'
+}
+
+interface props {
+  bookTrade: BookTrade
+}
+const TradeBookCard: React.FC<props> = ({ bookTrade }) => {
+  const { fromUser, toUser, status, offeredBook, desiredBook, updatedAt } =
+    bookTrade
+  const { enqueueSnackbar } = useSnackbar()
+  const { user } = useUser()
+  const { acceptBookTrade, rejectBookTrade } = useBookTrade()
+  const isUserRequest = user?._id === (bookTrade.fromUser as User)._id
+
+  const userRequestPending = isUserRequest && status == 'pending'
+  const userRequestAnswered = isUserRequest && status != 'pending'
+  const requestedUserPending = !isUserRequest && status == 'pending'
+  const requestedUserAnswered = !isUserRequest && status != 'pending'
+  const statusSituation = {
+    pending: 'solicitou',
+    accepted: 'aceitou',
+    rejected: 'rejeitou'
+  }
+  const currentAvatarUser =
+    userRequestPending || requestedUserAnswered ? user : fromUser
+
+  const handleRejectSolicitation = async () => {
+    rejectBookTrade(bookTrade._id)
+      .then(() => {
+        enqueueSnackbar('Solicitação rejeitada com sucesso.', {
+          variant: 'success',
+          anchorOrigin
+        })
+      })
+      .catch(() => {
+        enqueueSnackbar(
+          'Não foi possivel rejeitar a solicitação da troca deste livro.',
+          {
+            variant: 'error',
+            anchorOrigin
+          }
+        )
+      })
+  }
+
+  const handleAcceptSolicitation = async () => {
+    acceptBookTrade(bookTrade._id)
+      .then(() => {
+        enqueueSnackbar('Solicitação aceite com sucesso.', {
+          variant: 'success',
+          anchorOrigin
+        })
+      })
+      .catch(() => {
+        enqueueSnackbar(
+          'Não foi possivel aceitar a solicitação da troca deste livro.',
+          {
+            variant: 'error',
+            anchorOrigin
+          }
+        )
+      })
+  }
+
+  return (
+    <>
+      <Stack direction="row" gap={2} my={2} px="24px">
+        <Box>
+          <Avatar
+            sx={{ width: '50px', height: '50px' }}
+            alt={currentAvatarUser?.firstName}
+            src={currentAvatarUser?.avatar}
+          />
+        </Box>
+        <Stack direction="column" gap="4px">
+          <Typography>
+            <strong>
+              {userRequestPending || requestedUserAnswered
+                ? 'Você'
+                : `${fromUser.firstName} ${fromUser.lastName}`}
+            </strong>{' '}
+            {statusSituation[status]} uma troca do livro
+            <strong>
+              {' '}
+              {offeredBook.title} ({offeredBook.author})
+            </strong>{' '}
+            para o livro
+            <strong>
+              {' '}
+              {desiredBook.title} ({desiredBook.author})
+            </strong>
+            .
+          </Typography>
+          <Typography>{moment(updatedAt).fromNow()}</Typography>
+          {requestedUserPending && (
+            <Stack direction="row" gap="8px" mt={2}>
+              <Button onClick={handleAcceptSolicitation} variant="contained">
+                Aceitar
+              </Button>
+              <Button variant="outlined" onClick={handleRejectSolicitation}>
+                {' '}
+                Rejeitar{' '}
+              </Button>
+            </Stack>
+          )}
+        </Stack>
+      </Stack>
+      <Divider />
+    </>
+  )
+}
+
+export default TradeBookCard
